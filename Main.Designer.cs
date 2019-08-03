268f1ba5a9fd35bb2452165a26f1ebcd8f6b12d7
gregstein <shock-feeling@hotmail.fr>
2019-08-03 18:50:25 +0000

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MSGitRepos
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Nextbtn = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.Prevbtn = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.kryptonPanel1.Controls.Add(this.flowLayoutPanel1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(654, 436);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel2.Controls.Add(this.tableLayoutPanel1);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 380);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(654, 56);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 322F));
+            this.tableLayoutPanel1.Controls.Add(this.Nextbtn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Prevbtn, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(648, 51);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Nextbtn
+            // 
+            this.Nextbtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Nextbtn.Location = new System.Drawing.Point(329, 3);
+            this.Nextbtn.Name = "Nextbtn";
+            this.Nextbtn.Size = new System.Drawing.Size(316, 45);
+            this.Nextbtn.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Nextbtn.StateDisabled.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Nextbtn.StateNormal.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nextbtn.StatePressed.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Nextbtn.StateTracking.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Nextbtn.TabIndex = 2;
+            this.Nextbtn.Values.Text = "Next";
+            this.Nextbtn.Click += new System.EventHandler(this.Nextbtn_Click);
+            // 
+            // Prevbtn
+            // 
+            this.Prevbtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Prevbtn.Enabled = false;
+            this.Prevbtn.Location = new System.Drawing.Point(3, 3);
+            this.Prevbtn.Name = "Prevbtn";
+            this.Prevbtn.Size = new System.Drawing.Size(320, 45);
+            this.Prevbtn.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Prevbtn.StateDisabled.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Prevbtn.StateNormal.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Prevbtn.StatePressed.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Prevbtn.StateTracking.Content.ShortText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold);
+            this.Prevbtn.TabIndex = 1;
+            this.Prevbtn.Values.Text = "Previous";
+            this.Prevbtn.Click += new System.EventHandler(this.Prevbtn_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(654, 392);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::MSGitRepos.Properties.Resources.loading;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 377);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 436);
+            this.Controls.Add(this.kryptonPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(672, 483);
+            this.Name = "Main";
+            this.Text = "MSGitRepos";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton Nextbtn;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton Prevbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+using Newtonsoft.Json;
+namespace MSGitRepos
+{
+    public partial class Main : KryptonForm
+    {
+        public bool validated = false;
+        public string _repostr;
+        public string RepoName;
+        public string RepoDesc;
+        public string RepoStar;
+        public string RepoIssue;
+        public string RepoUserN;
+        public string RepoAvatar;
+        public string RepoURL;
+        public int CurPage;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void Main_Load(object sender, EventArgs e)
+        {
+
+            Task<int> GrabJson = JsonMain(@"https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=", 1);
+            int result = await GrabJson;
+
+            if (validated)
+                await Task.Run(() => JsonParser());
+
+        }
+      
+
+        #region Json Tasks
+        public async void JsonParser()
+        {
+            try
+            {   
+                //Clear all
+                Invoke(new Action(() => {flowLayoutPanel1.Controls.Clear();}));
+                await Task.Delay(100);
+                //Setting Control Count
+                int n = 30;
+                KryptonLinkLabel[] links = new KryptonLinkLabel[n];
+                KryptonLabel[] labeldesc = new KryptonLabel[n];
+                KryptonLabel[] stargazers = new KryptonLabel[n];
+                KryptonLabel[] openissues = new KryptonLabel[n];
+                PictureBox[] avatar = new PictureBox[n];
+                KryptonLabel[] username = new KryptonLabel[n];
+                var jObject = Newtonsoft.Json.Linq.JObject.Parse(_repostr);
+                for (int i = 0; i <= 29; i++)
+                {
+
+                        RepoName = (string)jObject["items"][i]["full_name"];
+                        RepoDesc = (string)jObject["items"][i]["description"];
+                        RepoDesc = RepoDesc == null ? string.Empty : RepoDesc.Substring(0, Math.Min(40, RepoDesc.Length)) + " ...";
+                        RepoStar = (string)jObject["items"][i]["stargazers_count"];
+                        RepoIssue = (string)jObject["items"][i]["open_issues_count"];
+                        RepoAvatar = (string)jObject["items"][i]["owner"]["avatar_url"];
+                        RepoUserN = (string)jObject["items"][i]["owner"]["login"];
+                        RepoURL = (string)jObject["items"][i]["owner"]["html_url"];
+                        links[i] = new KryptonLinkLabel();
+                        labeldesc[i] = new KryptonLabel();
+                        stargazers[i] = new KryptonLabel();
+                        openissues[i] = new KryptonLabel();
+                        avatar[i] = new PictureBox();
+                        username[i] = new KryptonLabel();
+                        Invoke(new Action(() =>
+                        {
+                            //Repo Title
+                            links[i].Text = RepoName;
+                            links[i].Click += new EventHandler(button_Click);
+                            links[i].StateCommon.ShortText.Font = new Font(links[i].Font, FontStyle.Bold);
+                            links[i].StateNormal.ShortText.Font = new Font(links[i].Font, FontStyle.Bold);
+                            links[i].StateNormal.ShortText.Color1 = Color.Blue;
+                            FontFamily fontFamily = new FontFamily("Arial");
+                            Font font = new Font(fontFamily, 20, FontStyle.Regular, GraphicsUnit.Pixel);
+                            links[i].StateNormal.ShortText.Font = font;
+                            links[i].StateCommon.ShortText.Font = font;
+                            //Repo Description
+                            labeldesc[i].Text = RepoDesc;
+                            labeldesc[i].StateNormal.ShortText.Color1 = Color.Black;
+                            //Icon + Repo Stars
+                            PictureBox starico = new PictureBox();
+                            starico.Size = new System.Drawing.Size(16, 16);
+                            starico.Image = MSGitRepos.Properties.Resources.starico;
+                            starico.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                            stargazers[i].Text = RepoStar;
+                            //Icon + Repo Issues
+                            PictureBox issueico = new PictureBox();
+                            issueico.Size = new System.Drawing.Size(16, 16);
+                            issueico.Image = MSGitRepos.Properties.Resources.issue;
+                            issueico.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                            openissues[i].Text = RepoIssue;
+                            //Username & Avatar
+                            KryptonLabel bydev = new KryptonLabel();
+                            bydev.Text = "Built by";
+
+                            avatar[i].Size = new System.Drawing.Size(16, 16);
+                            avatar[i].ImageLocation = RepoAvatar;
+                            avatar[i].SizeMode = PictureBoxSizeMode.StretchImage;
+
+                            username[i].Text = RepoUserN;
+                            //Merge Controls
+                            
+                            flowLayoutPanel1.SetFlowBreak(links[i], true);
+                            flowLayoutPanel1.Controls.Add(links[i]);
+                            
+
+                            flowLayoutPanel1.SetFlowBreak(labeldesc[i], true);
+                            flowLayoutPanel1.Controls.Add(labeldesc[i]);
+
+                            flowLayoutPanel1.Controls.Add(starico);
+                            flowLayoutPanel1.Controls.Add(stargazers[i]);
+
+                            flowLayoutPanel1.Controls.Add(issueico);
+                            flowLayoutPanel1.Controls.Add(openissues[i]);
+
+                            flowLayoutPanel1.Controls.Add(bydev);
+                            flowLayoutPanel1.Controls.Add(avatar[i]);
+
+                            flowLayoutPanel1.SetFlowBreak(username[i], true);
+                            flowLayoutPanel1.Controls.Add(username[i]);
+
+                            
+                            
+                        }));
+        
+                    
+                    if (i == 29)
+                    {
+                        KryptonSeparator splitter = new KryptonSeparator();
+                        Invoke(new Action(() => { flowLayoutPanel1.Controls.Add(splitter);}));
+                        break;
+                    }
+                        
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //Nothing
+            }
+            
+            
+        }
+        async Task<int> JsonMain(string jsonURL, int pageNUM)
+        {
+            try
+            {
+                CurPage = pageNUM;
+                WebClient Clt = new WebClient();
+                Clt.Headers.Add("user-agent", "MSGitRepos");
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                ServicePointManager.Expect100Continue = false; ServicePointManager.MaxServicePointIdleTime = 0;
+                _repostr = await Clt.DownloadStringTaskAsync(jsonURL + pageNUM.ToString());
+                validated = true;
+                return 0;
+            }
+            catch(Exception ex)
+            {
+                KryptonMessageBox.Show(ex.ToString());
+                validated = false;
+                CurPage = 1;
+                return 0;
+            }
+           
+        }
+        async Task<int> Preloader()
+        {
+            //Preload Effect
+            Invoke(new Action(() => { flowLayoutPanel1.Controls.Clear(); }));
+            PictureBox preloader = new PictureBox();
+            preloader.Size = new System.Drawing.Size(648, 377);
+            preloader.Image = MSGitRepos.Properties.Resources.loading;
+            preloader.SizeMode = PictureBoxSizeMode.CenterImage;
+            preloader.BackColor = Color.White;
+            Invoke(new Action(() => { flowLayoutPanel1.Controls.Add(preloader); }));
+            await Task.Delay(500);
+            return 0;
+        }
+        protected void button_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/" + (sender as KryptonLinkLabel).Text);
+            
+        }
+        #endregion
+
+        #region Navigation Events
+        private async void Nextbtn_Click(object sender, EventArgs e)
+        {
+            if(CurPage >= 1)
+            {
+                //Preload Effect
+              
+                Task<int> ImgLoad = Preloader();
+                int WaitL = await ImgLoad;
+
+                //Enable prev navigation
+                Prevbtn.Enabled = true;
+                //Parse Json with defined Pagination
+                Task<int> GrabJson = JsonMain(@"https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=", CurPage++);
+                int result = await GrabJson;
+
+                if (validated)
+                    await Task.Run(() => JsonParser());
+            }
+            CurPage++;
+            
+        }
+        private async void Prevbtn_Click(object sender, EventArgs e)
+        {
+            if (CurPage == 1)
+            {
+                Prevbtn.Enabled = false;
+            }
+            else
+            {
+                //Preload Effect
+                Task<int> ImgLoad = Preloader();
+                int WaitL = await ImgLoad;
+
+                
+                //Parse Json with defined Pagination
+                Task<int> GrabJson = JsonMain(@"https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=", CurPage--);
+                int result = await GrabJson;
+
+                if (validated)
+                    await Task.Run(() => JsonParser());
+            }
+            CurPage--;
+           
+            
+        }
+        #endregion
+
+
+
+
+
+
+
+        public CancellationToken token { get; set; }
+    }
+}
